b99a2df2a6e6515eb621731bcdd67a50c8a36c20
williams-derek-j <williams-derek-j@proton.me>
2025-12-05 16:35:02 -0800

    final commit

---
+++ .idea/server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log('incoming request...')
+
+    res.setHeader('Content-Type', 'text/html')
+
+    let path = './views'
+
+    switch(req.url) {
+        case '/':
+            path += '/index.html'
+            res.statusCode = 200
+            break
+        case '/about':
+            path += '/about.html'
+            res.statusCode = 200
+            break
+        case '/contact-me':
+            path += '/contact-me.html';
+            res.statusCode = 200;
+            break
+        default:
+            path += '/404.html';
+            res.statusCode = 404;
+    }
+
+    fs.readFile(path, (error, data) => {
+        if (error) {
+            console.log(error)
+            res.end();
+        } else {
+            res.end(data);
+        }
+    })
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('server listening on 3000... ')
+});
+
